be7ebecd597bb6b002ab4686ced1b914e11f419a
Rafael de Luca <deluca1712@gmail.com>
2023-06-30 16:59:45 -0300

    Example of Map with a external file.csv

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,54 +1,64 @@
 package application;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
+import java.io.PrintStream;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Program {
+public class Program {	
+	
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);			
+		
 
-		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the full path of the file: " );
+		
+		String absolutePath = new File("").getAbsolutePath();
+		String path = absolutePath + File.separator  + "\\src\\application\\votes.csv";
+		
+		// String path = input.nextLine();		
+		
+		// order by natural order of adding the elements to the Map
+		Map<String, Integer> votes = new LinkedHashMap<String, Integer>();
 
-		Map<String, Integer> votes = new LinkedHashMap<>();
-
-		System.out.print("Enter file full path: ");
-		// c:\temp\votos.csv
-		String path = input.nextLine();
-
+		System.out.println("\n");
+		
 		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
 
 			String line = bufferedReader.readLine();
 			while (line != null) {
-
 				String[] fields = line.split(",");
-				String name = fields[0];
-				int count = Integer.parseInt(fields[1]);
+				String userName = fields[0];
+				Integer count = Integer.valueOf(fields[1]);
 
-				if (votes.containsKey(name)) {
-					int votesUntilNow = votes.get(name);
-					votes.put(name, count + votesUntilNow);
+				if (votes.containsKey(userName) == true) {
+					int volesUntilNow = votes.get(userName);
+					votes.put(userName, count + volesUntilNow);
 				}
-
+				// in case it is a new username 
 				else {
-					votes.put(name, count);
+					votes.put(userName, count);
 				}
 
 				line = bufferedReader.readLine();
 			}
-
-			for (String nickname : votes.keySet()) {
-				System.out.println(nickname + ": " + votes.get(nickname));
+			
+			for(String userName : votes.keySet()) {
+				System.out.println(userName + ": " + votes.get(userName));
 			}
 
-		} catch (IOException erro) {
-			System.out.println("Erro: " + erro.getMessage());
+		} catch (IOException error) {
+			System.out.println("Error: " + error.getLocalizedMessage());
 		}
 
 		input.close();
+
 	}
+
 }
